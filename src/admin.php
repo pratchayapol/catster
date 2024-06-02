ebd2067103d6a3176f75fb1726eeadb1b32f31c0
เพ็ญพิสุทธิ์ พงษ์สถิตย์พัฒน์ <phenphisut.po@rmuti.ac.th>
2024-06-02 18:25:58 +0700

    update

--- src/admin.php
+++ src/admin.php
@@ -6,72 +6,64 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> แอดมิน </title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<style>
+  body {
+    font-family: "Lato", sans-serif;
+  }
+
+  .sidenav {
+    height: 100%;
+    width: 250px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #eee;
+    overflow-x: hidden;
+    padding-top: 20px;
+  }
+
+  .sidenav a {
+    padding: 6px 8px 6px 16px;
+    text-decoration: none;
+    color: #818181;
+    display: block;
+  }
+
+  .sidenav a:hover {
+    color: #f1f1f1;
+  }
+
+  .main {
+    margin-left: 250px; /* Same as the width of the sidenav */
+    padding: 0px 10px;
+  }
+
+  @media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+    .sidenav a {font-size: 18px;}
+  }
+</style>
 </head>
 <body>
-    <div class="sidenav">
-        <a href="admin.php">หน้าหลัก</a>
-        <a href="shelter.php">สถานพักพิง</a>
-        <a href="employees.php">พนักงาน</a>
-        <a href="members.php">สมาชิก</a>
-        <a href="types.php">ประเภท</a>
-        <a href="products.php">สินค้า</a>
-        <a href="#vaccines">วัคซีน</a>
-        <a href="logout.php"><button type="button" class="btn mt-2" style="background-color: #FFA559;"> ออกจากระบบ </button></a>
+
+  <div class="sidenav">
+    <div class="navbar-brand" style="text-align:center;">
+      <p> Catster </p>
     </div>
-    <div class="main">
-        <h2> หน้าหลักแอดมิน </h2>
-        <p> ยินดีต้อนรับ :
-            <?php
-                if(isset($_SESSION['firstname'])){
-                    echo "<span class='text-secondary'>";
-                    echo $_SESSION['firstname'] . " " . $_SESSION['lastname'];
-                    echo "</span>";
-                }
-            ?>
-        </p>
-        <h1 style="text-align: center;"> คิดไว้ว่าน่าจะเป็น Dashboard </h1>
-    </div>
+  </div>
+
+  <div class="main">
+    <h2>Sidebar</h2>
+    <p>This sidebar is of full height (100%) and always shown.</p>
+    <p>Scroll down the page to see the result.</p>
+    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  </div>
+   
 </body>
-</html>
-
-<style>
-.sidenav {
-  height: 100%;
-  width: 160px;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background-color: #111;
-  overflow-x: hidden;
-  padding-top: 20px;
-}
-
-.sidenav a {
-  padding: 6px 8px 6px 16px;
-  text-decoration: none;
-  font-size: 25px;
-  color: #818181;
-  display: block;
-}
-
-.sidenav a:hover {
-  color: #f1f1f1;
-}
-
-.main {
-  margin-left: 160px; /* Same as the width of the sidenav */
-  padding: 0px 10px;
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-</style>
+</html> 
