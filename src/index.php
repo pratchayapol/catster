2f46183294accb60b204229ab6db5d119ab73774
pratchayapol <108515090+pratchayapol@users.noreply.github.com>
2024-06-25 07:37:45 +0700

    Update index.php by pratchayapol

--- src/index.php
+++ src/index.php
@@ -9,8 +9,7 @@
     <meta charset="utf-8">
     <title>Catster - หน้าแรก</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="" name="keywords">
-    <meta content="" name="description">
+
 
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
@@ -30,7 +29,7 @@
     <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Template Stylesheet -->
     <link href="assets/css/style.css" rel="stylesheet">
@@ -41,11 +40,7 @@
     <?php include('include/header.php'); ?>
     <div class="container-xxl bg-white p-0">
         <!-- Spinner Start -->
-        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-                <span class="sr-only">กำลังโหลด...</span>
-            </div>
-        </div>
+       
         <!-- Spinner End -->
 
 
